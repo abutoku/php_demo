3e5142eafd3c4447f7b20d89ac3e95fe942f6b35
abutoku <ystkz2021@gmail.com>
2021-12-19 15:32:50 +0900

    add upfile img page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//------メニューボタンの動き-----------------------------//
+//---メニューボタンの動き-----------------------------//
 
 //初期設定
 $('#mask').hide(); //メニュー背景隠す
@@ -16,4 +16,33 @@ $('#hamburger').on('click', function () {
 $('#mask').on('click', function () {
   $('#mask').hide(); //メニュー背景隠す
   $('#menu_contents').hide(); //メニュー中身隠す
+});
+
+//---写真を登録するフォームの動き----------------------//
+
+//初期設定
+$('#img_mask').hide(); //メニュー背景隠す
+$('#img_upload_inner').hide(); //メニュー中身隠す
+
+//写真変更を押したら発動
+$('#img_edit').on('click', function () {
+  $('#img_mask').show(); //メニュー背景表示
+  $('#img_upload_inner').show(); //メニュー中身表示
+});
+
+//背景をクリックしたら発動
+$('#img_mask').on('click', function () {
+  $('#img_mask').hide(); //メニュー背景隠す
+  $('#img_upload_inner').hide(); //メニュー中身隠す
+});
+
+
+//---プロフィール写真が選択されたらプレビューを表示-----//
+
+$('#new_profile_img').on('change', function (e) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    $("#demo_img").attr('src', e.target.result);
+  }
+  reader.readAsDataURL(e.target.files[0]);
 });
